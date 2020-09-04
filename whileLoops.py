f94877d3915a2f59f82da9a1642f9283268bf44c
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-03 20:09:56 -0500

    working on python

--- whileLoops.py
+++ whileLoops.py
@@ -13,4 +13,48 @@ def print_prime_factors(number):
             factor += 1
     return "Done"
 
+
 print_prime_factors(100)
+
+
+def is_power_of_two(n):
+    # Check if the number can be divided by two without a remainder
+    if n == 0:
+        return False
+    while n % 2 == 0:
+        n = n / 2
+    # If after dividing by two the number is 1, it's a power of two
+    if n == 1:
+        return True
+    return False
+
+
+print(is_power_of_two(0))  # Should be False
+print(is_power_of_two(1))  # Should be True
+print(is_power_of_two(8))  # Should be True
+print(is_power_of_two(9))  # Should be False
+
+
+def sum_divisors(n):
+    sum = 1
+    # Return the sum of all divisors of n, not including n
+    div = 2
+    if n == 0:
+        return 0
+
+    while div < n / 2 + 1:
+        if n % div == 0:
+            sum += div
+        div += 1
+
+    return sum
+
+
+print(sum_divisors(0))
+# 0
+print(sum_divisors(3))  # Should sum of 1
+# 1
+print(sum_divisors(36))  # Should sum of 1+2+3+4+6+9+12+18
+# 55
+print(sum_divisors(102))  # Should be sum of 2+3+6+17+34+51
+# 114
