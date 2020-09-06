5cd52667a9d484f350af1e0670d065ad6088d823
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-05 21:38:34 -0500

    working on python

--- for-loops.py
+++ for-loops.py
@@ -13,3 +13,26 @@ for x in range(1, 11):
 
 for i in range(0, 101, 7):
     print(i)
+
+
+def retry(operation, attempts):
+    for n in range(attempts):
+        if operation():
+            print("Attempt " + str(n) + " succeeded")
+            break
+        else:
+            print("Attempt " + str(n) + " failed")
+
+
+def create_user(args):
+    pass
+
+
+retry(create_user, 3)
+
+
+def stop_service(args):
+    pass
+
+
+retry(stop_service, 5)
