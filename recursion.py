c2427f10cd218acc0cfbded653cfc2dbefd5e670
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-07 21:09:29 -0500

    working on python

--- recursion.py
+++ recursion.py
@@ -11,3 +11,17 @@ def is_power_of(number, base):
 print(is_power_of(8, 2))  # Should be True
 print(is_power_of(64, 4))  # Should be True
 print(is_power_of(70, 10))  # Should be False
+
+
+def count_users(group):
+    count = 0
+    for member in get_members(group):
+        count += 1
+        if is_group(member):
+            count += count_users(member) - 1
+    return count
+
+
+print(count_users("sales"))  # Should be 3
+print(count_users("engineering"))  # Should be 8
+print(count_users("everyone"))  # Should be 18<\code>
