be9f779ca5c153ff5692878f6426fabd5f6e5770
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-09 22:59:57 -0500

    working on python

--- recursion.py
+++ recursion.py
@@ -53,3 +53,21 @@ def show_letters(word):
 
 show_letters("Hello")
 # Should print one line per letter
+
+import math
+
+
+def digits(n):
+    count = 0
+    if n == 0:
+        return 1
+    while (n > 0):
+        count += 1
+        n = math.floor(n / 10)
+    return count
+
+
+print(digits(25))  # Should print 2
+print(digits(144))  # Should print 3
+print(digits(1000))  # Should print 4
+print(digits(0))  # Should print 1
