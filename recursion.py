d3f84dfd51252714622d40e6a08c77bcae03b7bd
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-09 23:01:56 -0500

    working on python

--- recursion.py
+++ recursion.py
@@ -108,3 +108,23 @@ def counter(start, stop):
 print(counter(1, 10))  # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
 print(counter(2, 1))  # Should be "Counting down: 2,1"
 print(counter(5, 5))  # Should be "Counting up: 5"
+
+
+def loop(start, stop, step):
+    return_string = ""
+    if step == 0:
+        step = 1
+    if start > stop:
+        step = abs(step) * -1
+    else:
+        step = abs(step)
+    for count in range(start, stop, step):
+        return_string += str(count) + " "
+    return return_string.strip()
+
+
+print(loop(11, 2, 3))  # Should be 11 8 5
+print(loop(1, 5, 0))  # Should be 1 2 3 4
+print(loop(-1, -2, 0))  # Should be -1
+print(loop(10, 25, -2))  # Should be 10 12 14 16 18 20 22 24
+print(loop(1, 1, 1))  # Should be empty
