f6406490a16c924ceb1033879d3737ff915a70a7
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-09 23:01:10 -0500

    working on python

--- recursion.py
+++ recursion.py
@@ -81,4 +81,30 @@ def multiplication_table(start, stop):
 
 
 multiplication_table(1, 3)
+
+
 # Should print the multiplication table shown above
+
+def counter(start, stop):
+    x = start
+    if start > stop:
+        return_string = "Counting down: "
+        while x >= stop:
+            return_string += str(x)
+            if x != stop:
+                return_string += ","
+            x -= 1
+
+    else:
+        return_string = "Counting up: "
+        while x <= stop:
+            return_string += str(x)
+            if x != stop:
+                return_string += ","
+            x += 1
+    return return_string
+
+
+print(counter(1, 10))  # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
+print(counter(2, 1))  # Should be "Counting down: 2,1"
+print(counter(5, 5))  # Should be "Counting up: 5"
