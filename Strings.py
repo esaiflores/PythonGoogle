a3e8c601f277fa5e09b2274b0e6beb19563d3658
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-10 21:27:59 -0500

    working on python

--- Strings.py
+++ Strings.py
@@ -21,11 +21,25 @@ print(is_palindrome("Never Odd or Even"))  # Should be True
 print(is_palindrome("abc"))  # Should be False
 print(is_palindrome("kayak"))  # Should be True
 
+
 def convert_distance(miles):
     km = miles * 1.6
-    result = "{} miles equals {:.1f} km".format(miles,km)
+    result = "{} miles equals {:.1f} km".format(miles, km)
     return result
 
-print(convert_distance(12)) # Should be: 12 miles equals 19.2 km
-print(convert_distance(5.5)) # Should be: 5.5 miles equals 8.8 km
-print(convert_distance(11)) # Should be: 11 miles equals 17.6 km
+
+print(convert_distance(12))  # Should be: 12 miles equals 19.2 km
+print(convert_distance(5.5))  # Should be: 5.5 miles equals 8.8 km
+print(convert_distance(11))  # Should be: 11 miles equals 17.6 km
+
+
+def nametag(first_name, last_name):
+    return ("{} {}.".format(first_name, last_name[0]))
+
+
+print(nametag("Jane", "Smith"))
+# Should display "Jane S."
+print(nametag("Francesco", "Rinaldi"))
+# Should display "Francesco R."
+print(nametag("Jean-Luc", "Grand-Pierre"))
+# Should display "Jean-Luc G."
