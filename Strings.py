519831c4e24991e56ed2f14cfdd4ad8d15e64916
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-10 21:27:25 -0500

    working on python

--- Strings.py
+++ Strings.py
@@ -20,3 +20,12 @@ def is_palindrome(input_string):
 print(is_palindrome("Never Odd or Even"))  # Should be True
 print(is_palindrome("abc"))  # Should be False
 print(is_palindrome("kayak"))  # Should be True
+
+def convert_distance(miles):
+    km = miles * 1.6
+    result = "{} miles equals {:.1f} km".format(miles,km)
+    return result
+
+print(convert_distance(12)) # Should be: 12 miles equals 19.2 km
+print(convert_distance(5.5)) # Should be: 5.5 miles equals 8.8 km
+print(convert_distance(11)) # Should be: 11 miles equals 17.6 km
