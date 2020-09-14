7e175ac76ea3d6cd3a63a725fbd9ab6e67f1b6c3
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-13 23:22:13 -0500

    working on python

--- Assessment.py
+++ Assessment.py
@@ -51,3 +51,17 @@ def highlight_word(sentence, word):
 print(highlight_word("Have a nice day", "nice"))
 print(highlight_word("Shhh, don't be so loud!", "loud"))
 print(highlight_word("Automating with Python is fun", "fun"))
+
+
+def combine_lists(list1, list2):
+    # Generate a new list containing the elements of list2
+    for i in range(len(list1) - 1, -1, -1):
+        list2.append(list1[i])
+    return list2
+    # Followed by the elements of list1 in reverse order
+
+
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
