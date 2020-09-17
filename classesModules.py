c8e3ef331244301410eec901b06aa576e5755030
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-16 23:14:02 -0500

    working on python classes and modules practice

--- classesModules.py
+++ classesModules.py
@@ -9,3 +9,17 @@ fido = Dog()
 fido.years = 3
 print(fido.dog_years())
 
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self):
+        # Should return "hi, my name is " followed by the name of the Person.
+        return "hi, my name is {}".format(self.name)
+
+
+# Create a new instance with a name of your choice
+some_person = Person("Foysal")
+# Call the greeting method
+print(some_person.greeting())
