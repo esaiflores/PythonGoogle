a2e2a5c41cffd5ace7f4c74adeefcff967ebd3c1
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-22 23:09:25 -0500

    working on python classes and modules practice

--- AllTogether.py
+++ AllTogether.py
@@ -25,3 +25,12 @@ class Event:
         self.type = event_type
         self.machine = machine_name
         self.user = user
+
+        events = [
+            Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+            Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+            Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+            Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
+            Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
+            Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
+        ]
