b18ff4e06be5b550d4fec62f40a2ed771f978092
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-22 23:09:45 -0500

    working on python classes and modules practice

--- AllTogether.py
+++ AllTogether.py
@@ -1,6 +1,7 @@
 def get_event_date(event):
     return event.date
 
+
 def current_users(events):
     events.sort(key=get_event_date)
     machines = {}
@@ -13,11 +14,13 @@ def current_users(events):
             machines[event.machine].remove(event.user)
     return machines
 
+
 def generate_report(machines):
     for machine, users in machines.items():
         if len(users) > 0:
             user_list = ", ".join(users)
             print("{}: {}".format(machine, user_list))
+
 
 class Event:
     def __init__(self, event_date, event_type, machine_name, user):
@@ -34,3 +37,8 @@ class Event:
             Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
             Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
         ]
+
+        users = current_users(events)
+
+
+print(users)
