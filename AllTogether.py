916691005a1505f427694fb492da62e238b85c63
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-22 23:09:08 -0500

    working on python classes and modules practice

--- AllTogether.py
+++ AllTogether.py
@@ -18,3 +18,10 @@ def generate_report(machines):
         if len(users) > 0:
             user_list = ", ".join(users)
             print("{}: {}".format(machine, user_list))
+
+class Event:
+    def __init__(self, event_date, event_type, machine_name, user):
+        self.date = event_date
+        self.type = event_type
+        self.machine = machine_name
+        self.user = user
